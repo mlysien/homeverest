297771fcf6bc6239e61b852edddc9f239d93592b
Mateusz Łysień <mateusz.lysien@homeverest.pl>
2026-05-11 23:11:11 +0200

    fix: correct base path in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -14,7 +14,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mlysien.github.io/homeverest',
-  base: '/homeverest',
+  base: '/homeverest/',
   output: 'static',
   outDir: 'dist',
   integrations: [
